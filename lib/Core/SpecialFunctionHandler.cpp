9b8971aa9c5b5d944d3816fb2830cec89d8e318c
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2022-12-02 19:28:03 +0800

    track pointers in models

--- lib/Core/SpecialFunctionHandler.cpp
+++ lib/Core/SpecialFunctionHandler.cpp
@@ -548,10 +548,10 @@ void SpecialFunctionHandler::trackPointer(ExecutionState &state,
   else
     type = "POINTER";
 
-  if (source_loc.find("/klee", 0) == std::string::npos) {
-    std::string log_message = source_loc + " : " + type + " : " + res + "\n";
-    klee_log_pointer(log_message.c_str());
-  }
+
+  std::string log_message = source_loc + " : " + type + " : " + res + "\n";
+  klee_log_pointer(log_message.c_str());
+
 
 }
 
